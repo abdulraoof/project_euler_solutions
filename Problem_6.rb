98b2235c940b5ab80cd54f8f6f1524ea4f216901
arabi <arabi@interadez.(none)>
2012-12-04 20:36:20 +0530

    Added missed questions

--- Problem_6.rb
+++ Problem_6.rb
@@ -1,3 +1,7 @@
+=begin
+Find the difference between the sum of the squares of the 
+first one hundred natural numbers and the square of the sum.
+=end
 
 sum1 = sum2 = 0
 (1..100).each { |x,y| sum1 += x*x; sum2 += x  }
--- Problem_9.rb
+++ Problem_9.rb
@@ -1,3 +1,8 @@
+=begin
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+=end	
+
 (1..998).each do |x|
 	(1..998).each do |y|
 		998.downto(1) do |z|
