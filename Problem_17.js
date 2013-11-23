ff65b4faac0e22cc6d864136e3163f4a5fb9cbf7
abdulraoof <mailtoraoof@gmail.com>
2013-11-23 13:07:04 +0530

    Working fine.Optimization pending

--- Problem_17.js
+++ Problem_17.js
@@ -2,7 +2,7 @@ var ontToNineteen = ["one","two","three","four","five","six","seven","eight","ni
 				  "twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
 
 var tens = ["twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"];
-var hundreds = ["hundred"];
+var hundreds = ["onehundred"];
 
 function makeHundreds(){
 	for(i=1; i<10; i++){
@@ -14,7 +14,7 @@ makeHundreds();
 var k = 0;
 for(var i=0;i<8;i++){
 	numberStack[k] = tens[i];
-	console.log(numberStack[k]);
+	//console.log(numberStack[k]);
 	k++; 
 	for(var j=0;j<9;j++){
 		numberStack[k] = tens[i]+ontToNineteen[j];
@@ -27,14 +27,15 @@ for(var i=0;i<8;i++){
 numberStackFull = ontToNineteen.concat(numberStack);
 var hundredToThousand=[];
 var m = 0;
-for(var x=0;x<10;x++){
+for(var x=0;x<9;x++){
 	hundredToThousand[m] = hundreds[x];
 	m++;
-	for(var y=0;y<100;y++){
+	for(var y=0;y<99;y++){
 		hundredToThousand[m] = hundreds[x]+"and"+numberStackFull[y];
-		console.log(hundredToThousand[m]);
+	//	console.log(hundredToThousand[m]);
 		m++;
 	}
 }
 var oneToThousand = numberStackFull.concat(hundredToThousand);
-var result = oneToThousand.join("");
+var result = oneToThousand.join("").length + "onethousand".length;
+alert(result);
